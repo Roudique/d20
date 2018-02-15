66fe582652661ea3e78b7457d8cb3265e7804f90
Oleksandr Rudavka <roudique@gmail.com>
2018-02-14 19:51:34 -0500

    Initial Commit

---
+++ RanDice/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RanDice
+//
+//  Created by Oleksandr Rudavka on 2018-02-14.
+//  Copyright © 2018 roudique. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
